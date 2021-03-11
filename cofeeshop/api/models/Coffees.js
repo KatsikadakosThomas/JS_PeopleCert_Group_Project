5a9c3410639575bb63246eb7dc1fa2a4dc423a0e
KatsikadakosThomas <tkatsika@gmail.com>
2021-03-11 11:55:06 +0200

    made the coffee model

---
+++ cofeeshop/api/models/Coffees.js
@@ -0,0 +1,50 @@
+/**
+ * Coffees.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    name:{
+      type:'string',
+      required: true,
+      description: 'The name of the product',
+      example: "esspreso machiato lugo cappuccino",
+     },
+
+     Description: {
+      type: 'string',
+      required: true,
+      maxLength: 80,
+     },
+     
+     price: {
+      type:'number',
+      required:true,
+      description: "products price",
+      example: 1.65
+    }
+
+
+    
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+};
+
