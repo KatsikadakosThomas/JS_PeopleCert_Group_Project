c97de72e12d40e7fd226f54d00914fbf1d2d0146
KatsikadakosThomas <tkatsika@gmail.com>
2021-03-12 12:11:48 +0200

    made a new shop test

---
+++ cofeeshop/api/controllers/shop.js
@@ -0,0 +1,21 @@
+/**
+ * ShopController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+   exits:{
+       success:{
+           viewTemplatePath:'pages/shop'
+       },
+   },
+
+
+   fn: async function () {
+       return {};
+   }
+
+};
+
