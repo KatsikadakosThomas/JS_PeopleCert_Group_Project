e56dccdcd26d5878e76d04153823a7fdf5bbd0dd
KatsikadakosThomas <tkatsika@gmail.com>
2021-04-07 23:33:01 +0300

    Update routes.js

--- cofeeshop/config/routes.js
+++ cofeeshop/config/routes.js
@@ -14,7 +14,7 @@
   //  ╚╩╝╚═╝╚═╝╩  ╩ ╩╚═╝╚═╝╚═╝
   "GET /": { action: "view-homepage-or-redirect" },
   "GET /welcome/:unused?": { action: "dashboard/view-welcome" },
-  "GET /admin/adminwelcome": {action: "dashboard/adminwelcome"},
+  "GET /adminwelcome": {action: "dashboard/adminwelcome"},
 
   "GET /faq": { action: "view-faq" },
   "GET /legal/terms": { action: "legal/view-terms" },
