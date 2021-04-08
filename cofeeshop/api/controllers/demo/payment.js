76cafbf7904978f76be0c5bed088c70d09a949fb
Fanis <fanis.kolokythas@gmail.com>
2021-04-08 23:19:58 +0300

    payment controller
    payment route controller and ejs

---
+++ cofeeshop/api/controllers/demo/payment.js
@@ -0,0 +1,27 @@
+module.exports = {
+
+
+    friendlyName: 'View adminwelcome page',
+  
+  
+    description: 'Display the dashboard "adminwelcome" page.',
+  
+  
+    exits: {
+  
+      success: {
+        viewTemplatePath: 'pages/payment/payment',
+        description: 'Display the payment page'
+      },
+  
+    },
+  
+  
+    fn: async function () {
+  
+      return {};
+  
+    }
+  
+  
+  };
