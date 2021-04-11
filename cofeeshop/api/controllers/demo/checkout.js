a16f5c11b69b93e910fcf672f822891831919339
KatsikadakosThomas <tkatsika@gmail.com>
2021-04-12 02:12:33 +0300

    total price checkout works

--- cofeeshop/api/controllers/demo/checkout.js
+++ cofeeshop/api/controllers/demo/checkout.js
@@ -4,12 +4,11 @@ module.exports = {
 
 fn: async function() {
       
-  // var amount= await Orders.find().where({id:1})
-  console.log(this.req.session.order.id);
-  var orders = await OrderDetails.find().where({ordersID:this.req.session.order.id})
-  console.log('orders log');
-  console.log(orders);
 
+  //get all orderDetails with session order id
+  var orders = await OrderDetails.find().where({ordersID:this.req.session.order.id})
+
+  //loop through all orderDetails object and add the result to finalprice
 	let arr = [];
 	for(let i = 0; i <orders.length; i++){
 		arr.push(orders[i].price);
@@ -18,6 +17,12 @@ fn: async function() {
  var finalprice = arr.reduce((a, b) => a + b, 0)
 console.log(finalprice)
 
+//insert total price to base
+var finalPriceDB=await Orders.update(this.req.session.order.id).set({totalPrice:finalprice}).fetch()
+console.log("final price");
+console.log(finalPriceDB);
+
+//make payment intent object for stipe
   var payment={
     payment_method_types: ['card'],
     line_items: [
@@ -34,13 +39,13 @@ console.log(finalprice)
     ],
     mode: 'payment',
     success_url: 'http://localhost:1337/success',
-    cancel_url: 'http://localhost:1337/demo/populate',
+    cancel_url: 'http://localhost:1337/demo/homepage',
   }
-  
+  console.log(Math.ceil(finalprice*100));
   payment.line_items[0].price_data.unit_amount = Math.ceil(finalprice*100);
   
-
-      const session = await stripe.checkout.sessions.create(payment);
+//make payment
+const session = await stripe.checkout.sessions.create(payment);
     
     return this.res.json({ id: session.id });
     
